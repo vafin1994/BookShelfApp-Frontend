ae6e61eef6be69e4dfb6d6695d910fc1202cd8ca
Oleksandr Vafin <vafinalex@gmail.com>
2026-05-10 12:06:21 +0200

    Add table to display a book list

--- src/app/book-list/book-list.ts
+++ src/app/book-list/book-list.ts
@@ -3,15 +3,23 @@ import { BookService } from '../services/book';
 import { PageResponse } from '../interfaces/pageResponse';
 import { Book } from '../interfaces/book';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { finalize } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-book-list',
-  imports: [],
+  imports: [MatTableModule, MatProgressSpinnerModule],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
 export class BookList implements OnInit {
   booksList: WritableSignal<Book[]> = signal<Book[]>([]);
+  isLoading = signal<boolean>(false);
+  error = signal<string | null>(null);
+
+  displayedColumns = ['title', 'author', 'genre', 'publishingYear'];
+
   private bookService = inject(BookService);
   private destroyRef = inject(DestroyRef);
 
@@ -20,12 +28,22 @@ export class BookList implements OnInit {
   }
 
   getAllBooks() {
+    this.isLoading.set(true);
+    this.error.set(null);
     this.bookService
       .getAllBooks()
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((response: PageResponse<Book>) => {
-        this.booksList.set(response.content);
-        console.log('Books: ', this.booksList);
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        finalize(() => this.isLoading.set(false)),
+      )
+      .subscribe({
+        next: (response: PageResponse<Book>) => {
+          this.booksList.set(response.content);
+          console.log('Books: ', this.booksList);
+        },
+        error: (error) => {
+          this.error.set('Failed to get books');
+        },
       });
   }
 }
