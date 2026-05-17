e9099c3276511c10c873a163f8cdac7435ae636c
Oleksandr Vafin <vafinalex@gmail.com>
2026-05-17 13:27:36 +0200

    WIP Author list: Add Create Author form

--- src/app/author-list/author-list.ts
+++ src/app/author-list/author-list.ts
@@ -1,10 +1,17 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { Author } from '../interfaces/author';
+import { Author, AuthorRequest } from '../interfaces/author';
 import { AuthorService } from '../services/author';
 import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
 import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-author-list',
@@ -18,14 +25,21 @@ import { MatIcon } from '@angular/material/icon';
     MatButton,
     MatIcon,
     MatIconButton,
+    ReactiveFormsModule,
   ],
   templateUrl: './author-list.html',
   styleUrl: './author-list.css',
 })
 export class AuthorList implements OnInit {
   private authorService: AuthorService = inject(AuthorService);
+
   public authors = signal<Author[]>([]);
   public editedAuthor = signal<number | null>(null);
+
+  public authorForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    country: new FormControl('', [Validators.required]),
+  });
 
   ngOnInit() {
     this.getAuthorsList();
@@ -37,7 +51,22 @@ export class AuthorList implements OnInit {
     });
   }
 
-  public addAuthor() {}
+  public addAuthor() {
+    const author: AuthorRequest = {
+      name: this.authorForm.value.name,
+      country: this.authorForm.value.country,
+    };
+    this.authorService.createAuthor(author).subscribe({
+      next: (res) => {
+        this.authors.update((authors) => [...authors, res]);
+        this.authorForm.reset();
+      },
+      error: (err) => {
+        // TODO add error indicator
+        console.log(err);
+      },
+    });
+  }
 
   public editAuthor(id: number | undefined) {
     if (id) {
